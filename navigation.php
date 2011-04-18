f8d1b7dfcda55868e04c75b6e9a081ba1143eb4b
David H <david@haystackproject.com>
2011-04-18 04:46:31 -0700

    Added latest Tumblr blag post headline linkage with JSON and stuff.

--- navigation.php
+++ navigation.php
@@ -11,8 +11,26 @@
 	<a href="creations.php">&nbsp;david creates&nbsp;</a>
 	<br />
 	<br />
-	<a href="http://huertanix.tumblr.com">&nbsp;david blags&nbsp;</a>
-	<br />
-	<br />
 	<a href="contact.php">&nbsp;contact david&nbsp;</a>
+	<br />
+	<div class="currentStatuses">
+		<b>blag.dh:</b>
+		<script type="text/javascript">
+			// Check for tumblebeasts
+			if (tumblr_api_read)
+			{		
+	    		// Grab latest blag post headline
+	    		document.write('<a href="' + tumblr_api_read['posts'][0]['url-with-slug'] + '">&nbsp;' 
+					+ tumblr_api_read['posts'][0]['link-text'] +'&nbsp;</a>');
+			}
+			else
+			{
+				document.write('brb tumblebeasts');
+			}
+		</script>
+		<noscript>
+			<a href="http://huertanix.tumblr.com/">Visit Blag.dh</a>
+		</noscript>
+		<!-- TODO: Redo this in PHP for Lynx users...-->
+	</div>
 </nav>
